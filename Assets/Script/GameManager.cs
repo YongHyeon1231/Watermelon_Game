8e60ef1a9ae8c7a0b6e2c20363ee3acf11637be3
PYH <dydgustmdfl@gmail.com>
2026-02-02 16:25:44 +0900

    [add] 다음 행성을 미리 결정하고 UI에 표시
    GameManager 논리 순서 변경
    1. Start() : 게임 시작하자마자 첫 번째 '다음 행성'을 미리 준비
    2. SetObject() :
    준비된 nextObject를 마우스 위치에 소환
    즉시 다음 턴에 쓸 행성을 새로 뽑음
    새로 뽑은 행성의 이미지를 UI에 반양
    3. 안전장치 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,19 +1,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> planets = new List<GameObject>();
     public GameObject NextObject;
     public GameObject Clone;
+    public TextMeshProUGUI scoreText;
+
+    public Image nextPlanetImage;
 
     float timeCount = 0;
+    private int score = 0;
 
     public static GameManager instance;
 
     public bool isGameOver = false;
-    public int score = 0;
 
     void Awake()
     {
@@ -24,6 +29,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        StartSetting();
+        SelectNextObject(); // 다음 행성 미리 뽑기
         SetObject();
     }
 
@@ -50,7 +57,7 @@ public class GameManager : MonoBehaviour
     public void AddScore(int amount)
     {
         score += amount;
-        Debug.Log("Score: " + score);
+        scoreText.text = score.ToString();
     }
 
     public void GameOver()
@@ -62,13 +69,16 @@ public class GameManager : MonoBehaviour
 
     // 물체가 마우스 X 좌표에 따라오게 만들기
     void currentObjectPos() {
+        if (Clone == null) return;
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Clone.transform.position = new Vector3(Mathf.Clamp(mousePos.x, -3.5f, 3.5f), 4.2f, 0);
     }
 
     void currentobjectCondition() {
+        if (Clone == null) return;
         Clone.GetComponent<CircleCollider2D>().enabled = true;
         Clone.GetComponent<Rigidbody2D>().gravityScale = 1;
+        Clone = null; // 참조 해제
     }
 
     void SelectNextObject() {
@@ -77,8 +87,28 @@ public class GameManager : MonoBehaviour
     }
 
     void SetObject() {
-        SelectNextObject();
+        if (NextObject == null) SelectNextObject();
+        // 1. 대기 중인 행성 소환
         Clone = Instantiate(NextObject);
         Clone.GetComponent<Planet>().gameManager = this;
+
+        // 2. 다음 행성 미리 뽑기
+        SelectNextObject();
+
+        // 3. UI 갱신 (다음 행성 이미지 보여주기)
+        if (nextPlanetImage != null) {
+            // 프리팹에 SpriteRenderer가 있다고 가정
+            SpriteRenderer sr = NextObject.GetComponent<SpriteRenderer>();
+            if (sr != null) {
+                nextPlanetImage.sprite = sr.sprite;
+            }
+        }
+    }
+
+    void StartSetting() {
+        Time.timeScale = 1;
+        isGameOver = false;
+        score = 0;
+        scoreText.text = "0";
     }
 }
